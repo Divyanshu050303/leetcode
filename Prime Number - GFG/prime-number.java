3e04f7dffcb497fc61a8cc7f8496cad94344216d
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2023-08-22 11:00:27 +0530

    Added solution - LeetHub

--- Prime Number - GFG/prime-number.java
+++ Prime Number - GFG/prime-number.java
@@ -22,12 +22,10 @@ class GFG
 class Solution{
     static int isPrime(int N){
          if(N == 1) return 0;
-        
-        int half = N/2;
+    
         for(int i = 2; i*i <= N; i++){
             if(N%i == 0) return 0;
         }
-        
         return 1;
     }
 }
